37c9dc1094f692aa6f70c06092cb65b7048aff2d
pcamilojunior <paulo.camilo.junior@outsystems.com>
2024-01-22 22:50:54 +0200

    Added the files to android only to use Instant App

---
+++ src/android/instant-app/src/main/java/com/test/instantapp/instant_app/InstantAppActivity.java
@@ -0,0 +1,34 @@
+package com.test.instantapp.instant_app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class InstantAppActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.outsystems.com");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
