4a9ad99a7980fd5a03f15016383327679d7218cc
André Grillo <andregrillo@gmail.com>
2024-01-26 23:13:28 +0000

    Troubleshooting mabs path error

--- hooks/save_preference_in_config.js
+++ hooks/save_preference_in_config.js
@@ -1,6 +1,5 @@
 var fs = require('fs');
 var path = require('path');
-var xml2js = require('xml2js');
 
 module.exports = function(context) {
     var provisioningProfiles = context.opts.plugin.variables.PROVISIONING_PROFILES;
@@ -12,38 +11,19 @@ module.exports = function(context) {
 
     var configXmlPath = path.join(context.opts.projectRoot, 'config.xml');
 
-    fs.readFile(configXmlPath, 'utf8', function(err, xml) {
+    fs.readFile(configXmlPath, 'utf8', function(err, data) {
         if (err) {
             throw new Error('Failed to read config.xml: ' + err);
         }
 
-        var parser = new xml2js.Parser();
-        parser.parseString(xml, function(err, result) {
+        // Replace the placeholder with the actual value
+        var updatedData = data.replace('--PLACEHOLDER--', provisioningProfiles);
+
+        fs.writeFile(configXmlPath, updatedData, 'utf8', function(err) {
             if (err) {
-                throw new Error('Failed to parse config.xml: ' + err);
+                throw new Error('Failed to write to config.xml: ' + err);
             }
-
-            if (!result.widget.preference) {
-                result.widget.preference = [];
-            }
-
-            // Add the PROVISIONING_PROFILES preference
-            result.widget.preference.push({
-                '$': {
-                    name: 'PROVISIONING_PROFILES',
-                    value: provisioningProfiles
-                }
-            });
-
-            var builder = new xml2js.Builder();
-            var newXml = builder.buildObject(result);
-
-            fs.writeFile(configXmlPath, newXml, function(err) {
-                if (err) {
-                    throw new Error('Failed to write to config.xml: ' + err);
-                }
-                console.log('config.xml updated with PROVISIONING_PROFILES value.');
-            });
+            console.log('config.xml updated with PROVISIONING_PROFILES value.');
         });
     });
 };
