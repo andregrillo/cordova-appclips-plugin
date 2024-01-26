fa5948f18cd3ee43f5d59d0db5b7bdaa1f3ff110
André Grillo <andregrillo@gmail.com>
2024-01-26 22:16:03 +0000

    Troubleshooting mabs path error

--- hooks/updateCordovaBuildJs.js
+++ hooks/updateCordovaBuildJs.js
@@ -1,13 +1,9 @@
-
 var fs = require('fs');
 var path = require('path');
 var {getCordovaParameter, log} = require('./utils');
 var decode = require('decode-html');
-const preferences = require('cordova-plugin-ionic/preferences');
 
-function escapeRegExp(string) {
-  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-}
+console.log("Running hook to install NodeJS requirements");
 
 module.exports = function(context) {
     
@@ -26,33 +22,47 @@ module.exports = function(context) {
         'build.js'
     )
 
-//    const args = process.argv
+    // Install the desired package directly within your hook
+    var child_process = require('child_process');
+    var deferral = context.requireCordovaModule('q').defer(); // Use 'q' module from Cordova context
 
-//    var ppDecoded;
-//    for (const arg of args) {  
-//      if (arg.includes('PROVISIONING_PROFILES')){
-//        var stringArray = arg.split("=");
-//        ppDecoded = stringArray.slice(-1).pop();
-//      }
-//    }
+    var packageName = 'cordova-plugin-ionic/preferences'; // Replace with the name of the package you want to install
 
+    var output = child_process.exec('npm install ' + packageName, { cwd: __dirname }, function (error) {
+        if (error !== null) {
+            console.log('exec error: ' + error);
+            deferral.reject('npm installation failed');
+        } else {
+            // Now that the package is installed, you can use it in your Cordova JS hook
+            try {
+                const preferences = require(packageName);
 
-    var ppDecoded = preferences.get('PROVISIONING_PROFILES');
+                // Use 'preferences' as needed in your Cordova JS hook
+                var ppDecoded = preferences.get('PROVISIONING_PROFILES');
+                var ppObject = JSON.parse(ppDecoded.replace(/'/g, "\""));
+                var ppString = "";
 
-    var ppObject = JSON.parse(ppDecoded.replace(/'/g, "\""));
-    var ppString = "";
-    
-    //we iterate here so we can add multiple provisioning profiles to, in the future, add provisioning profiles for other extensions
-    Object.keys(ppObject).forEach(function (key) {
-        ppString += ", \n [ '" + key + "' ]: String('" + ppObject[key] + "')";
-        log('Trying to add provisioning profile with uuid "' + ppObject[key] + '" to bundleId "' + key + '"','success');
+                // Iterate to add multiple provisioning profiles
+                Object.keys(ppObject).forEach(function (key) {
+                    ppString += ", \n [ '" + key + "' ]: String('" + ppObject[key] + "')";
+                    log('Trying to add provisioning profile with uuid "' + ppObject[key] + '" to bundleId "' + key + '"','success');
+                });
+
+                var toReplace = "[ bundleIdentifier ]: String(buildOpts.provisioningProfile)";
+                var regexp = new RegExp(escapeRegExp(toReplace), 'g');
+                var plistContents = fs.readFileSync(buildJsPath, 'utf8');
+                plistContents = plistContents.replace(regexp, toReplace + ppString);
+                fs.writeFileSync(buildJsPath, plistContents);
+
+                log('Successfully edited build.js', 'success');
+                
+                deferral.resolve();
+            } catch (err) {
+                console.log('Error requiring ' + packageName + ':', err);
+                deferral.reject('Error requiring ' + packageName);
+            }
+        }
     });
 
-    var toReplace = "[ bundleIdentifier ]: String(buildOpts.provisioningProfile)";
-    var regexp = new RegExp(escapeRegExp(toReplace), 'g');
-    var plistContents = fs.readFileSync(buildJsPath, 'utf8');
-    plistContents = plistContents.replace(regexp, toReplace + ppString);
-    fs.writeFileSync(buildJsPath, plistContents);
-
-    log('Successfully edited build.js', 'success');
+    return deferral.promise;
 }
