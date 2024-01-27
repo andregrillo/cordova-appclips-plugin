e54a88eb5a3a8e300a6f69531b77142c5722c7c1
André Grillo <andregrillo@gmail.com>
2024-01-27 00:13:10 +0000

    Troubleshooting mabs path error

--- hooks/updateCordovaBuildJs.js
+++ hooks/updateCordovaBuildJs.js
@@ -40,38 +40,33 @@ function escapeRegExp(string) {
 }
 
 module.exports = function(context) {
-    
-    log(
-    '🦄 Running updateCordovaBuildJS hook, adding provisioning profiles to build.js 🦄 ',
-    'start'
-    );
+    log('🦄 Running updateCordovaBuildJS hook, adding provisioning profiles to build.js 🦄 ', 'start');
 
     var iosFolder = context.opts.cordova.project
-    ? context.opts.cordova.project.root
-    : path.join(context.opts.projectRoot, 'platforms/ios/');
+        ? context.opts.cordova.project.root
+        : path.join(context.opts.projectRoot, 'platforms/ios/');
+    var buildJsPath = path.join(iosFolder, 'cordova/lib', 'build.js');
 
-    var buildJsPath = path.join(
-        iosFolder,
-        'cordova/lib',
-        'build.js'
-    )
+    readProvisioningProfilesPreference(context.opts.projectRoot)
+        .then(ppDecoded => {
+            var ppObject = JSON.parse(ppDecoded.replace(/'/g, "\""));
+            var ppString = "";
+            
+            Object.keys(ppObject).forEach(function (key) {
+                ppString += ", \n [ '" + key + "' ]: String('" + ppObject[key] + "')";
+                log('Trying to add provisioning profile with uuid "' + ppObject[key] + '" to bundleId "' + key + '"', 'success');
+            });
 
-    const ppDecoded = readProvisioningProfilesPreference(context.opts.projectRoot);
+            var toReplace = "[ bundleIdentifier ]: String(buildOpts.provisioningProfile)";
+            var regexp = new RegExp(escapeRegExp(toReplace), 'g');
+            var plistContents = fs.readFileSync(buildJsPath, 'utf8');
+            plistContents = plistContents.replace(regexp, toReplace + ppString);
+            fs.writeFileSync(buildJsPath, plistContents);
 
-    var ppObject = JSON.parse(ppDecoded.replace(/'/g, "\""));
-    var ppString = "";
-    
-    //we iterate here so we can add multiple provisioning profiles to, in the future, add provisioning profiles for other extensions
-    Object.keys(ppObject).forEach(function (key) {
-        ppString += ", \n [ '" + key + "' ]: String('" + ppObject[key] + "')";
-        log('Trying to add provisioning profile with uuid "' + ppObject[key] + '" to bundleId "' + key + '"','success');
-    });
-
-    var toReplace = "[ bundleIdentifier ]: String(buildOpts.provisioningProfile)";
-    var regexp = new RegExp(escapeRegExp(toReplace), 'g');
-    var plistContents = fs.readFileSync(buildJsPath, 'utf8');
-    plistContents = plistContents.replace(regexp, toReplace + ppString);
-    fs.writeFileSync(buildJsPath, plistContents);
-
-    log('Successfully edited build.js', 'success');
-}
+            log('Successfully edited build.js', 'success');
+        })
+        .catch(error => {
+            console.error('Error reading PROVISIONING_PROFILES:', error);
+            // Handle the error appropriately
+        });
+};
