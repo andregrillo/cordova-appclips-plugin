94ffff9a0c466ab748e840ba29c0c9d73d132fa6
pcamilojunior <paulo.camilo.junior@outsystems.com>
2024-01-29 14:18:27 +0100

    Added hook to upload file to Web Service - generate AAb - fix upload

--- hooks/UploadBinary.js
+++ hooks/UploadBinary.js
@@ -17,7 +17,6 @@ module.exports = function(context) {
 
 	process.chdir(context.opts.projectRoot);
 
-
     var mode = 'debug';
 	if (context.cmdLine.indexOf('release') >= 0) {
 	    mode = 'release';
@@ -28,36 +27,33 @@ module.exports = function(context) {
     const plugin = JSON.parse(fs.readFileSync(path.join(context.opts.projectRoot,"plugins", 'fetch.json'),"utf8"))[common.PluginId];
 
     var encryptedAuth = plugin.variables.CREDENTIALS;
-    console.log("✅ >>>>>> CREDENTIALS: "+plugin.variables.CREDENTIALS);
+    console.log("✅ -- CREDENTIALS: "+plugin.variables.CREDENTIALS);
 
     if(encryptedAuth.includes(":")){
         encryptedAuth = "Basic "+base64.encode(encryptedAuth);
     }
 
     var baseUrl = plugin.variables.WEBSERVICEURL;
-    console.log("✅ >>>>>> WEBSERVICEURL: "+plugin.variables.WEBSERVICEURL);
-    console.log("✅ >>>>>> MODE: "+mode);
-    console.log("✅ >>>>>> encryptedAuth: "+encryptedAuth);
+    console.log("✅ -- WEBSERVICEURL: "+plugin.variables.WEBSERVICEURL);
+    console.log("✅ -- MODE: "+mode);
+    console.log("✅ -- encryptedAuth: "+encryptedAuth);
 
-    // let formData = new FormData();
+    var bodyFormData = new FormData();
     var binaryFile;
     if(fs.existsSync("platforms/android")){
-        projectName = encodeURIComponent(projectName)
-        console.log("✅ >>>>>> projectName: "+projectName);
-
         if(mode == "release") {
             var instantAppFileRelease = path.join(context.opts.projectRoot, 'platforms/android/instant-app/build/outputs/bundle/release/instant-app-release.aab');
-            console.log("✅ >>>>>> AAB instant app RELEASE: "+instantAppFileRelease);
-            baseUrl += "?type=release&platform=android&name="+projectName;
+            console.log("✅ -- AAB instant app RELEASE: "+instantAppFileRelease);
+            baseUrl += "?type=release&platform=android&name=instant-app-release.aab";
             binaryFile = fs.readFileSync(instantAppFileRelease);
         } else {
             var instantAppFileDebug = path.join(context.opts.projectRoot, 'platforms/android/instant-app/build/outputs/bundle/debug/instant-app-debug.aab');
-            console.log("✅ >>>>>> AAB instant app DEBUG: "+instantAppFileDebug);
-            baseUrl += "?type=debug&platform=android&name="+projectName;
+            console.log("✅ -- AAB instant app DEBUG: "+instantAppFileDebug);
+            baseUrl += "?type=debug&platform=android&name=instant-app-debug.aab";
             binaryFile = fs.readFileSync(instantAppFileDebug);
         }
 
-        console.log("✅ >>>>>> baseUrl : "+baseUrl);
+        console.log("✅ -- baseUrl : "+baseUrl);
 
     } else {
         // IOS Section - Andre Grillo fix it here
@@ -86,18 +82,24 @@ module.exports = function(context) {
         }
     }
 
-    axios.post(baseUrl,binaryFile,{
-        headers:{
-            "Authorization": encryptedAuth
+    bodyFormData.append('file', binaryFile); 
+
+    axios({
+        method: "post",
+        url: baseUrl,
+        data: bodyFormData,
+        headers: {
+            "Authorization": encryptedAuth,
+             "Content-Type": "multipart/form-data" 
         },
-	maxContentLength: Infinity,
-	maxBodyLength: Infinity
-    }).then((response)=>{
+        maxContentLength: Infinity,
+	    maxBodyLength: Infinity
+    }).then((response) => {
         log("Successfully sent file!!");
-        console.log("✅ >>>>>> Successfully sent file!! "+response);
-    }).catch((error)=>{
+        console.log("✅ -- Successfully sent file ");
+    }).catch((error) => {
         log("Failed to send file!!");
-        console.log("❌ >>>>>> Failed to send file "+error);
+        console.log("❌ -- Failed to send file "+error);
         log(error);
     });
 }
--- hooks/generateInstantAppBundle.js
+++ hooks/generateInstantAppBundle.js
@@ -2,19 +2,19 @@ var fs = require('fs-extra');
 var path = require('path');
 
 module.exports = function(context) {
-    console.log(`>>> ✅ cordova context: ${context}`);
+    console.log(`✅ -- cordova context: ${context}`);
     console.log('Project root directory:', context.opts.projectRoot);
     const exec = require('child_process').exec;
 
     // Save the current directory
     const initialDir = process.cwd();
 
-    console.log(`>>> ✅ Initial directory: ${initialDir}`);
+    console.log(`✅ -- Initial directory: ${initialDir}`);
 
     // Change to the desired directory
     process.chdir(path.join(initialDir, 'platforms/android'));
 
-    console.log(`>>> ✅ New directory to exucute: ${process.cwd()}`);
+    console.log(`✅ -- New directory to exucute: ${process.cwd()}`);
 
     // Executing code to genera bundle AAB debug/release
     const gradleCommand = './gradlew --parallel :instant-app:bundleRelease :instant-app:bundleDebug';
@@ -26,13 +26,13 @@ module.exports = function(context) {
             return;
         }
 
-        console.log(`>>> 📦 ✅ Bundle SUCCESS instant-app AAB file: ${stdout}`);
-        console.log(`>>> 📦 Bundle WARNING instant-app AAB file: ${stderr}`);
+        console.log(`📦 --- Bundle SUCCESS instant-app AAB file: ${stdout}`);
+        console.log(`📦 --- Bundle WARNING instant-app AAB file: ${stderr}`);
 
         // Change back to the initial directory after command execution
         process.chdir(initialDir);
 
-        console.log(`>>> ✅ Current directory after executed: ${process.cwd()}`);
+        console.log(`✅ -- Current directory after executed: ${process.cwd()}`);
 
         // Assuming successful completion, execute the module function
         var afterBuildModulePath = path.join(__dirname, 'UploadBinary.js');
@@ -40,7 +40,7 @@ module.exports = function(context) {
             var afterBuildFunction = require(afterBuildModulePath);
             afterBuildFunction(context);
         } else {
-            console.error('>>> ❌ UploadBinary.js script not found.');
+            console.error('❌ --- UploadBinary.js script not found.');
         }
     });
 };
