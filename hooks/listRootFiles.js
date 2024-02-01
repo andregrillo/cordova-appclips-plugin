6b404e91f50892ae750918a8c0a10b4a1f14e9c5
André Grillo <andregrillo@gmail.com>
2024-02-01 16:28:42 +0000

    Testing upload path

--- hooks/listRootFiles.js
+++ hooks/listRootFiles.js
@@ -1,18 +1,39 @@
+// File: hooks/listRootFiles.js
+
 const fs = require('fs');
 const path = require('path');
 
 module.exports = function(context) {
     const projectRoot = context.opts.projectRoot;
 
-    console.log('Listing files and subfolders at the root of the project folder:');
+    // Function to list contents of a directory
+    function listDirectoryContents(dirPath, title) {
+        console.log(`\nListing ${title}:`);
 
-    // Read the root directory
-    fs.readdir(projectRoot, { withFileTypes: true }, (err, dirents) => {
-        if (err) throw err;
+        fs.readdir(dirPath, { withFileTypes: true }, (err, dirents) => {
+            if (err) {
+                console.log(`Error reading directory: ${dirPath}`);
+                console.log(err.message);
+                return;
+            }
 
-        dirents.forEach(dirent => {
-            const direntType = dirent.isDirectory() ? 'Directory' : 'File';
-            console.log(`${dirent.name} - ${direntType}`);
+            dirents.forEach(dirent => {
+                const direntType = dirent.isDirectory() ? 'Directory' : 'File';
+                console.log(`${dirent.name} - ${direntType}`);
+            });
         });
+    }
+
+    // List root directory contents
+    listDirectoryContents(projectRoot, 'files and subfolders at the root of the project folder');
+
+    // List platforms/ios directory contents, if it exists
+    const iosPlatformPath = path.join(projectRoot, 'platforms', 'ios');
+    fs.access(iosPlatformPath, fs.constants.F_OK, (err) => {
+        if (err) {
+            console.log('\nNo iOS platform directory found. Skipping iOS platform listing.');
+            return;
+        }
+        listDirectoryContents(iosPlatformPath, 'files and subfolders in the platforms/ios directory');
     });
 };
