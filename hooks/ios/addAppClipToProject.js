8aa7555fd1d87c9f28ebbd312d7783319cb5b4f1
André Grillo <andregrillo@gmail.com>
2024-02-01 22:18:50 +0000

    Testing upload path

--- hooks/ios/addAppClipToProject.js
+++ hooks/ios/addAppClipToProject.js
@@ -9,6 +9,18 @@ var xcode = require('xcode');
 var Config = require("./config");
 var {log, getCordovaParameter} = require('./utils')
 
+function getProjectName() {
+    var config = fs.readFileSync('config.xml').toString();
+    var parseString = require('xml2js').parseString;
+    var name;
+    parseString(config, function (err, result) {
+        name = result.widget.name.toString();
+        const r = /\B\s+|\s+\B/g; // Removes trailing and leading spaces
+        name = name.replace(r, '');
+    });
+    return name || null;
+}
+
 function replacePlaceholdersInPlist(plistPath, placeHolderValues) {
     var plistContents = fs.readFileSync(plistPath, 'utf8');
     for (var i = 0; i < placeHolderValues.length; i++) {
@@ -332,6 +344,48 @@ module.exports = function (context) {
 
     run();
   });
+
+
+const projectPath = path.join(projectRoot, 'platforms/ios', getProjectName() + '.xcodeproj', 'project.pbxproj');
+const appclipPath = path.join(projectRoot, 'platforms/ios/CDVAppClips');
+let pbxprojContents = fs.readFileSync(projectPath, 'utf8');
+pbxprojContents = pbxprojContents.replace('AppDelegate2.m', 'AppDelegate.m');
+pbxprojContents = pbxprojContents.replace('AppDelegate2.h', 'AppDelegate.h');
+pbxprojContents = pbxprojContents.replace('main2.m', 'main.m');
+
+// Write the modified contents back to the file
+fs.writeFileSync(projectPath, pbxprojContents);
+console.log('✅ Renamed all files for CDVAppClips target.');
+
+//Rename the files back to normal
+const filesToRename = {
+    'main2.m': 'main.m',
+    'AppDelegate2.m': 'AppDelegate.m',
+    'AppDelegate2.h': 'AppDelegate.h',
+  };
+
+  fs.readdir(appclipPath, (err, files) => {
+    if (err) {
+      console.error('Error reading AppClip directory:', err);
+      return;
+    }
+
+    files.forEach(file => {
+      if (filesToRename[file]) {
+        const oldPath = path.join(appclipPath, file);
+        const newPath = path.join(appclipPath, filesToRename[file]);
+
+        fs.rename(oldPath, newPath, (err) => {
+          if (err) {
+            console.error(`🚨 Error renaming file ${file}:`, err);
+          } else {
+            console.log(`✅ Renamed ${file} to ${filesToRename[file]}`);
+          }
+        });
+      }
+    });
+  });
+
 
   return deferral.promise;
 };
