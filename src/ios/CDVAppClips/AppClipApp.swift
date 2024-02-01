7e73ee50c3f3cdc0155b8940e9c6b0538afe3d28
André Grillo <andregrillo@gmail.com>
2024-02-01 22:57:43 +0000

    Testing upload path

---
+++ src/ios/CDVAppClips/AppClipApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppClipApp.swift
+//  AppClip
+//
+//  Created by Andre Grillo on 18/01/2024.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+@main
+struct AppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
