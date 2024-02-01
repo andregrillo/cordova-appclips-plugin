ed616e47ffc91972ace9dd8da917ff6b5538354b
André Grillo <andregrillo@gmail.com>
2024-02-01 23:39:48 +0000

    Testing upload path

--- hooks/ios/remove_bridging_header_appclip.js
+++ hooks/ios/remove_bridging_header_appclip.js
@@ -16,6 +16,7 @@ function getProjectName() {
 
 module.exports = function (context) {
 
+    console.log('⭐️ Removing the SWIFT_OBJC_BRIDGING_HEADER from the App Clip Target');
     pbxPath = path.join(context.opts.projectRoot, 'platforms/ios/',getProjectName() + '.xcodeproj','project.pbxproj');
 
     let project = xcode.project(pbxPath);
@@ -23,8 +24,11 @@ module.exports = function (context) {
 
     Object.keys(project.pbxXCBuildConfigurationSection()).forEach((key) => {
         var config = project.pbxXCBuildConfigurationSection()[key];
+        console.log('⭐️ config: ' + config);
         if (typeof config === 'object' && config.buildSettings) {
+            console.log('⭐️ 1');
             if (config.buildSettings['PRODUCT_NAME'] === 'CDVAppClips') {
+                console.log('⭐️ Removing it!');
                 delete config.buildSettings['SWIFT_OBJC_BRIDGING_HEADER'];
                 delete config.buildSettings['SWIFT_OBJC_INTERFACE_HEADER_NAME'];
             }
