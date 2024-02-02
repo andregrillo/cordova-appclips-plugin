3afba212894cc6ff0323962fd4b48d0aefa17bbf
André Grillo <andregrillo@gmail.com>
2024-02-02 01:07:16 +0000

    Testing upload path

--- hooks/ios/ios_entitlements_fix.js
+++ hooks/ios/ios_entitlements_fix.js
@@ -50,7 +50,7 @@ module.exports = function(context) {
 
         // The string to search for
         const searchString = 'PRODUCT_NAME = "CDVAppClips";';
-        const replacementString = `PRODUCT_NAME = "CDVAppClips";\n\t\t\t\tCODE_SIGN_ENTITLEMENTS = "$(PROJECT_DIR)/CDVAppClips/CDVAppClips.entitlements";\n\t\t\t\t"DEVELOPMENT_TEAM[sdk=iphoneos*]" = ${teamId} ;\n\t\t\t\t"PROVISIONING_PROFILE_SPECIFIER[sdk=iphoneos*]" = ${provProf};\n\t\t\t\tPRODUCT_DISPLAY_NAME = "${appClipName}";`;
+        const replacementString = `PRODUCT_NAME = "CDVAppClips";\n\t\t\t\tCODE_SIGN_ENTITLEMENTS = "$(PROJECT_DIR)/CDVAppClips/CDVAppClips.entitlements";\n\t\t\t\t"DEVELOPMENT_TEAM[sdk=iphoneos*]" = ${teamId} ;\n\t\t\t\t"PROVISIONING_PROFILE_SPECIFIER[sdk=iphoneos*]" = ${provProf};\n\t\t\t\tPRODUCT_DISPLAY_NAME = "${appClipName}";\n\t\t\t\tSWIFT_OBJC_BRIDGING_HEADER = "";\n\t\t\t\tSWIFT_PRECOMPILE_BRIDGING_HEADER = NO;`;
 
         // Check if the string exists in the file
         if (pbxprojContents.includes(searchString)) {
