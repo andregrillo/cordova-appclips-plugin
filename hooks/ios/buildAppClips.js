3437c0cb3fce972e021d453b764f95b6de8bf8ef
André Grillo <andregrillo@gmail.com>
2024-02-02 10:38:03 +0000

    Testing upload path

--- hooks/ios/buildAppClips.js
+++ hooks/ios/buildAppClips.js
@@ -32,7 +32,9 @@ module.exports = function(context) {
                 console.log(cleanStdout);
 
                 // Step 1: Archive the build with verbose output
-                const archiveCommand = `xcodebuild archive -workspace AppInstantClips.xcworkspace -scheme CDVAppClips -configuration Debug -sdk iphoneos -archivePath build/Debug-iphoneos/AppClips.xcarchive -verbose`;
+                
+                const archiveCommand = `xcodebuild -scheme CDVAppClips -workspace AppInstantClips.xcworkspace clean archive -archivePath build/Debug-iphoneos/AppClips.xcarchive -verbose -sdk iphoneos`;
+                //const archiveCommand = `xcodebuild archive -workspace AppInstantClips.xcworkspace -scheme CDVAppClips -configuration Debug -sdk iphoneos -archivePath build/Debug-iphoneos/AppClips.xcarchive -verbose`;
                 //const archiveCommand = `xcodebuild -workspace AppInstantClips.xcworkspace -scheme CDVAppClips -configuration Debug -destination generic/platform=iOS -archivePath AppClips.xcarchive archive -verbose`;
                                        
                 exec(archiveCommand, { cwd: iosPlatformPath }, (archiveError, archiveStdout, archiveStderr) => {
